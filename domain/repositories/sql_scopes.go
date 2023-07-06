b648e26cb1d8412802bfe1e76120e1c550f45416
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-07-06 09:21:03 +0700

    fix: incorrect query

--- domain/repositories/sql_scopes.go
+++ domain/repositories/sql_scopes.go
@@ -9,7 +9,7 @@ import (
 
 func NotDeleted(timer timer.Timer, table schema.Tabler) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		query := fmt.Sprintf("%s.deleted_at == 0 OR %s.deleted_at > ?", table.TableName(), table.TableName())
+		query := fmt.Sprintf("%s.deleted_at = 0 OR %s.deleted_at > ?", table.TableName(), table.TableName())
 		return db.Where(query, timer.Now().UnixMilli())
 	}
 }
