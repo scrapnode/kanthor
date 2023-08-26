380e96a73dde8a13c003c90890fff8c08104921c
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-08-26 09:28:17 +0700

    ci(2023.826.928): ✨🐛🚨

---
+++ scripts/ci_trigger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+NOW=$(date +%Y.%-m%d.%-H%M)
+
+git commit --allow-empty -m "ci($NOW): ✨🐛🚨"
+
+TARGET=${1:-origin}
+echo "---------------------------"
+printf "Pushing... $NOW --> %s\n" "$TARGET"
+echo "---------------------------"
+git push "$TARGET"
