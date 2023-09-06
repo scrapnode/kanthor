dd69402b282229c3a0d04ccdf28b2144c9400446
Release Scheduler UAT <release.scheduler.uat@upmesh.io>
2023-09-06 08:51:34 +0700

    optimize: build faster with checksum

---
+++ scripts/ci_vet.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+CHECKSUM_NEW=$(find . -type f -name '*.go' -exec sha256sum {} \; | sort -k 2 | sha256sum | cut -d  ' ' -f1)
+CHECKSUM_OLD=$(cat ./checksum)
+
+if [ "$CHECKSUM_NEW" != "$CHECKSUM_OLD" ];
+then
+  echo "vetting ..."
+  # only vet on our packages
+  go list ./... | grep kanthor | go vet -v
+  find . -type f -name '*.go' -exec sha256sum {} \; | sort -k 2 | sha256sum | cut -d  ' ' -f1 > ./checksum
+fi
