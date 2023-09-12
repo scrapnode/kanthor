8c3edb47a69e86b0465250c80ca7c47b8797e3a4
Release Scheduler UAT <release.scheduler.uat@upmesh.io>
2023-09-12 14:32:43 +0700

    fix: nil pointer

--- domain/entities/metadata.go
+++ domain/entities/metadata.go
@@ -17,7 +17,9 @@ func (meta Metadata) Set(key, value string) {
 }
 
 func (meta Metadata) Merge(src map[string]string) {
-	for key, value := range src {
-		meta[key] = value
+	if len(src) > 0 {
+		for key, value := range src {
+			meta[key] = value
+		}
 	}
 }
