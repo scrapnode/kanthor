496e6d96f5df56706a3b068b74af19d9f6d1fdae
Release Scheduler UAT <release.scheduler.uat@upmesh.io>
2023-09-12 14:38:42 +0700

    refactor: only do cleanup task when we trigger CI

--- scripts/ci_trigger.sh
+++ scripts/ci_trigger.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+
+sh "$SCRIPT_DIR/../ci_vet.sh"
+sh "$SCRIPT_DIR/../gen_docs.sh"
+sh "$SCRIPT_DIR/../gen_ioc.sh"
+
+find . -type f -name 'checksum' -exec git add {} \;
+
 NOW=$(date +%Y.%-m%d.%-H%M)
 
 git commit --allow-empty -m "ci($NOW): ✨🐛🚨"
