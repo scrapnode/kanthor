db28b31bf09df4d083ba9eb3583a7cc3abbee613
Release Scheduler UAT <release.scheduler.uat@upmesh.io>
2023-11-05 13:04:16 +0700

    update: scripts

--- scripts/test_request.sh
+++ scripts/test_request.sh
@@ -3,6 +3,7 @@ set -e
 
 API_CREDS_PATH=${API_CREDS_PATH:-"/tmp"}
 API_ENDPOINT=${API_ENDPOINT:-"http://localhost:8180"}
+REQUEST_WAIT_TIME=${REQUEST_WAIT_TIME:-1}
 
 go run main.go migrate database up && go run main.go migrate datastore up
 go run main.go setup account kanthor_root_key --data=scripts/k6/httpbin.json --output="$API_CREDS_PATH/sdk.json"
@@ -12,6 +13,9 @@ TEST_USERNAME=$(cat $API_CREDS_PATH/sdk.json | jq -r '.credentials.username')
 TEST_PASSWORD=$(cat $API_CREDS_PATH/sdk.json | jq -r '.credentials.password')
 AUTH_TOKEN=$(echo -n "$TEST_USERNAME:$TEST_PASSWORD" | base64 -w 0)
 REQUEST_ID=$(uuidgen)
+
+echo "sleep $REQUEST_WAIT_TIME second(s)"
+sleep $REQUEST_WAIT_TIME
 
 curl -X PUT "$API_ENDPOINT/api/application/$TEST_APP_ID/message" \
     -H "Content-Type: application/json" \
