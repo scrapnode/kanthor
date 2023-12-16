eea05aae8ace596d419b4017e6a87c2ac9d22e04
Release Scheduler <release.scheduler@upmesh.io>
2023-12-16 10:47:15 +0700

    fix: middlewares

--- gateway/gin/middlewares/cors.go
+++ gateway/gin/middlewares/cors.go
@@ -11,8 +11,23 @@ func UseCors() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowAllOrigins:  true,
 		AllowCredentials: false,
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "Idempotency-Key", "X-Authorization-Engine", "X-Authorization-Workspace"},
-		MaxAge:           time.Hour * 12,
+		AllowMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"PATCH",
+			"DELETE",
+			"HEAD",
+			"OPTIONS"},
+		AllowHeaders: []string{
+			"Origin",
+			"Content-Length",
+			"Content-Type",
+			"Authorization",
+			"Idempotency-Key",
+			"X-Authorization-Engine",
+			"X-Authorization-Workspace",
+		},
+		MaxAge: time.Hour * 12,
 	})
 }
--- gateway/gin/middlewares/idempotency.go
+++ gateway/gin/middlewares/idempotency.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"context"
 	"net/http"
+	"slices"
 
 	"github.com/gin-gonic/gin"
 	"github.com/scrapnode/kanthor/gateway"
@@ -11,35 +12,38 @@ import (
 )
 
 var HeaderIdempotencyKey = "idempotency-key"
+var requires = []string{
+	http.MethodPost,
+	http.MethodPut,
+	http.MethodPatch,
+	http.MethodDelete,
+}
 
 func UseIdempotency(logger logging.Logger, engine idempotency.Idempotency) gin.HandlerFunc {
 	return func(ginctx *gin.Context) {
 		method := ginctx.Request.Method
-		bypass := method == http.MethodGet || method == http.MethodHead
-		if bypass {
-			logger.Debugw("bypass method", "method", method)
-			ginctx.Next()
-			return
-		}
+		required := slices.Contains(requires, method)
 
-		key := ginctx.GetHeader(HeaderIdempotencyKey)
-		if key == "" {
-			ginctx.AbortWithStatusJSON(http.StatusBadRequest, gateway.NewError("no idempotency key"))
-			return
-		}
+		if required {
+			key := ginctx.GetHeader(HeaderIdempotencyKey)
+			if key == "" {
+				ginctx.AbortWithStatusJSON(http.StatusBadRequest, gateway.NewError("no idempotency key"))
+				return
+			}
 
-		ctx := ginctx.MustGet(gateway.Ctx).(context.Context)
-		ok, err := engine.Validate(ctx, key)
-		if err != nil {
-			logger.Error(err)
-			ginctx.AbortWithStatusJSON(http.StatusInternalServerError, gateway.NewError("could not validate idempotency key"))
-			return
-		}
+			ctx := ginctx.MustGet(gateway.Ctx).(context.Context)
+			ok, err := engine.Validate(ctx, key)
+			if err != nil {
+				logger.Error(err)
+				ginctx.AbortWithStatusJSON(http.StatusInternalServerError, gateway.NewError("could not validate idempotency key"))
+				return
+			}
 
-		if !ok {
-			logger.Errorw("duplicated request", "method", method, "idempotency_key", key)
-			ginctx.AbortWithStatusJSON(http.StatusPreconditionFailed, gateway.NewError("duplicated request"))
-			return
+			if !ok {
+				logger.Errorw("duplicated request", "method", method, "idempotency_key", key)
+				ginctx.AbortWithStatusJSON(http.StatusPreconditionFailed, gateway.NewError("duplicated request"))
+				return
+			}
 		}
 
 		ginctx.Next()
