bc279b40775aa9e343515ca556740dffe60b9304
Release Scheduler <release.scheduler@upmesh.io>
2024-01-03 10:26:39 +0700

    fix: test scripts

--- scripts/test_request.sh
+++ scripts/test_request.sh
@@ -1,35 +1,53 @@
 #!/bin/sh
 set -e
 
-API_CREDS_PATH=${API_CREDS_PATH:-"/tmp"}
-API_ENDPOINT=${API_ENDPOINT:-"http://localhost:8180"}
+STORAGE_PATH=${STORAGE_PATH:-"/tmp"}
+PORTAL_AUTH_CREDENTIALS=${PORTAL_AUTH_CREDENTIALS:-"YWRtaW5Aa2FudGhvcmxhYnMuY29tOmNoYW5nZW1lbm93"}
+PORTAL_API_ENDPOINT=${PORTAL_API_ENDPOINT:-"http://localhost:8280/api"}
+SDK_API_ENDPOINT=${SDK_API_ENDPOINT:-"http://localhost:8180/api"}
 REQUEST_WAIT_TIME=${REQUEST_WAIT_TIME:-1}
 REQUEST_COUNT=${REQUEST_COUNT:-1}
+TEST_WORKSPACE_SNAPSHOT_PATH=${TEST_WORKSPACE_SNAPSHOT_PATH:-"scripts/k6/httpbin.json"}
 
 go run main.go migrate database up && go run main.go migrate datastore up
-go run main.go setup account admin@kanthorlabs.com --data=scripts/k6/httpbin.json --output="$API_CREDS_PATH/sdk.json"
 
-TEST_APP_ID=$(cat $API_CREDS_PATH/sdk.json | jq -r '.applications[0]')
-TEST_USERNAME=$(cat $API_CREDS_PATH/sdk.json | jq -r '.credentials.username')
-TEST_PASSWORD=$(cat $API_CREDS_PATH/sdk.json | jq -r '.credentials.password')
-AUTH_TOKEN=$(echo -n "$TEST_USERNAME:$TEST_PASSWORD" | base64 -w 0)
+# prepare new workspace with new application
 
-echo "sleep $REQUEST_WAIT_TIME second(s)"
-sleep $REQUEST_WAIT_TIME
+IDEMPTOTENCY_KEY_WORKSPACE_CREATE=$(uuidgen)
+curl -s -X POST "$PORTAL_API_ENDPOINT/workspace" \
+    -H "Content-Type: application/json" \
+    -H "Idempotency-Key: $IDEMPTOTENCY_KEY_WORKSPACE_CREATE" \
+    -H "Authorization: basic $PORTAL_AUTH_CREDENTIALS" \
+    -H 'Content-Type: application/json' \
+    -d '{"name": "test request"}' > "$STORAGE_PATH/workspace.json"
 
-echo "sending $REQUEST_COUNT messages ---> $TEST_APP_ID"
+TEST_WORKSPACE_ID=$(cat $STORAGE_PATH/workspace.json | jq -r '.id')
+jq '{snapshot: .}' $TEST_WORKSPACE_SNAPSHOT_PATH > "$STORAGE_PATH/workspace.snapshot.json"
+
+IDEMPTOTENCY_KEY_WORKSPACE_TRANSFER=$(uuidgen)
+curl -s -X POST "$PORTAL_API_ENDPOINT/workspace/$TEST_WORKSPACE_ID/transfer" \
+    -H "Content-Type: application/json" \
+    -H "Idempotency-Key: $IDEMPTOTENCY_KEY_WORKSPACE_TRANSFER" \
+    -H "Authorization: basic $PORTAL_AUTH_CREDENTIALS" \
+    -d @$STORAGE_PATH/workspace.snapshot.json > "$STORAGE_PATH/workspace.transfer.json"
+
+jq '{id: .app_id[0]}' "$STORAGE_PATH/workspace.transfer.json" > "$STORAGE_PATH/application.json"
+
+# only retrive the app id from trust source
+TEST_APP_ID=$(cat "$STORAGE_PATH/application.json" | jq -r '.id')
+echo "App ID: $TEST_APP_ID"
 
 for SEQ in $( seq 1 $REQUEST_COUNT )
 do
     IDEMPTOTENCY_KEY=$(uuidgen)
-    echo -n "\n--> $SEQ -> $IDEMPTOTENCY_KEY\n"
-    curl "$API_ENDPOINT/api/message" \
+    echo -n "$IDEMPTOTENCY_KEY/$SEQ -> $TEST_APP_ID\n"
+    curl -s -X POST "$SDK_API_ENDPOINT/message" \
         -H "Content-Type: application/json" \
         -H "Idempotency-Key: $IDEMPTOTENCY_KEY" \
-        -H "X-Authorization-Engine: sdk.internal" \
-        -H "Authorization: Basic $AUTH_TOKEN" \
-        --data-raw "{\"app_id\": \"$TEST_APP_ID\",\"type\":\"testing.traffic.request\",\"body\":{\"hello\":\"world\",\"seq\":$SEQ},\"headers\":{\"x-client\":\"curl\"}}"
+        -H "X-Authorization-Engine: ask" \
+        -H "X-Authorization-Workspace: $TEST_WORKSPACE_ID" \
+        -H "Authorization: Basic $PORTAL_AUTH_CREDENTIALS" \
+        -d "{\"app_id\": \"$TEST_APP_ID\",\"type\":\"testing.traffic.request\",\"body\":{\"hello\":\"world\",\"seq\":$SEQ},\"headers\":{\"x-client\":\"curl\"}}" > /dev/null
 done
 
-echo ""
-echo "sent $REQUEST_COUNT messages ---> $TEST_APP_ID"
+echo "App ID: $TEST_APP_ID | $REQUEST_COUNT messages"
