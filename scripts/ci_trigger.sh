5d0e9a9c2163a7bca12e1e439f1c40e01d4fa5da
Release Scheduler <release.scheduler@upmesh.io>
2024-01-04 19:56:06 +0700

    sync(sdk): 2024.104.1956

--- scripts/ci_trigger.sh
+++ scripts/ci_trigger.sh
@@ -8,7 +8,7 @@ sh "$SCRIPT_DIR/gen_ioc.sh"
 sh "$SCRIPT_DIR/ci_vet.sh"
 
 NOW=$(date +%Y.%-m%d.%-H%M)
-HAHSTAGS=${HAHSTAGS:-""}
+HAHSTAGS=${HAHSTAGS:-"#sdk"}
 
 git commit --allow-empty -m "ci($NOW): ✨🐛🚨 $HAHSTAGS"
 
