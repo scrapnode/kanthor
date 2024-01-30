f0cdc2f99b83cd121d3239444ec335e43b048dc4
Release Scheduler UAT <release.scheduler.uat@upmesh.io>
2024-01-30 11:25:33 +0700

    update: docker configurations

--- scripts/docker_reset.sh
+++ scripts/docker_reset.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )";
 docker compose -f docker-compose.yaml down
 docker volume prune -f
 
-docker compose up -d cache warehouse streaming migration-database migration-datastore
+docker compose up --build -d cache warehouse streaming migration-database migration-datastore
 docker compose ps
