118296207b2b735be1701980365d5589ef79c5ef
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2024-01-30 15:14:36 +0700

    ci(2024.130.1514): ✨🐛🚨 #sdk

--- scripts/test_request.sh
+++ scripts/test_request.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-STORAGE_PATH=${STORAGE_PATH:-"/tmp"}
+TEST_STORAGE_PATH=${TEST_STORAGE_PATH:-"/tmp"}
 PORTAL_AUTH_CREDENTIALS=${PORTAL_AUTH_CREDENTIALS:-"YWRtaW5Aa2FudGhvcmxhYnMuY29tOmNoYW5nZW1lbm93"}
 PORTAL_API_ENDPOINT=${PORTAL_API_ENDPOINT:-"http://localhost:8280/api"}
 SDK_API_ENDPOINT=${SDK_API_ENDPOINT:-"http://localhost:8180/api"}
@@ -19,10 +19,10 @@ curl -s -X POST "$PORTAL_API_ENDPOINT/workspace" \
     -H "Idempotency-Key: $IDEMPTOTENCY_KEY_WORKSPACE_CREATE" \
     -H "Authorization: basic $PORTAL_AUTH_CREDENTIALS" \
     -H 'Content-Type: application/json' \
-    -d "{\"name\": \"test workspace at $NOW\"}" > "$STORAGE_PATH/workspace.json"
+    -d "{\"name\": \"test workspace at $NOW\"}" > "$TEST_STORAGE_PATH/workspace.json"
 
-TEST_WORKSPACE_ID=$(cat $STORAGE_PATH/workspace.json | jq -r '.id')
-jq '{snapshot: .}' $TEST_WORKSPACE_SNAPSHOT_PATH > "$STORAGE_PATH/workspace.snapshot.json"
+TEST_WORKSPACE_ID=$(cat $TEST_STORAGE_PATH/workspace.json | jq -r '.id')
+jq '{snapshot: .}' $TEST_WORKSPACE_SNAPSHOT_PATH > "$TEST_STORAGE_PATH/workspace.snapshot.json"
 echo "Ws ID: $TEST_WORKSPACE_ID"
 
 IDEMPTOTENCY_KEY_WORKSPACE_TRANSFER=$(uuidgen)
@@ -30,12 +30,12 @@ curl -s -X POST "$PORTAL_API_ENDPOINT/workspace/$TEST_WORKSPACE_ID/transfer" \
     -H "Content-Type: application/json" \
     -H "Idempotency-Key: $IDEMPTOTENCY_KEY_WORKSPACE_TRANSFER" \
     -H "Authorization: basic $PORTAL_AUTH_CREDENTIALS" \
-    -d @$STORAGE_PATH/workspace.snapshot.json > "$STORAGE_PATH/workspace.transfer.json"
+    -d @$TEST_STORAGE_PATH/workspace.snapshot.json > "$TEST_STORAGE_PATH/workspace.transfer.json"
 
-jq '{id: .app_id[0]}' "$STORAGE_PATH/workspace.transfer.json" > "$STORAGE_PATH/application.json"
+jq '{id: .app_id[0]}' "$TEST_STORAGE_PATH/workspace.transfer.json" > "$TEST_STORAGE_PATH/application.json"
 
 # only retrive the app id from trust source
-TEST_APP_ID=$(cat "$STORAGE_PATH/application.json" | jq -r '.id')
+TEST_APP_ID=$(cat "$TEST_STORAGE_PATH/application.json" | jq -r '.id')
 echo "App ID: $TEST_APP_ID"
 
 for SEQ in $( seq 1 $REQUEST_COUNT )
